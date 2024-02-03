a74a65d29a9db88c21fc1d4120efc8bf44005589
sisay <sisaybekele735@gmail.com>
2024-02-03 22:17:33 +0300

    vite config file edited

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { build, defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// }
+// //   build: {
+// //   chunkSizeWarningLimit: 1500, // Set the limit to your desired value in kilobytes
+// // },
+
+// )
+
+// vite.config.js
+
+export default {
+  build: {
+    chunkSizeWarningLimit: 1500, // Set the limit to your desired value in kilobytes
+  },
   plugins: [react()],
-})
+};
